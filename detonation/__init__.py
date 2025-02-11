37ac4b713cd9cddc9efd479807a7b8acb5c6fdc0
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2025-02-11 18:11:54 +0100

    prepare_detonation

--- detonation/__init__.py
+++ detonation/__init__.py
@@ -1,3 +1,4 @@
 from .optim import *
+from .prepare import *
 from .repl import *
 from .util import *
---
+++ detonation/prepare.py
@@ -0,0 +1,44 @@
+import os
+import torch
+from torch.distributed.device_mesh import init_device_mesh
+from torch.distributed.fsdp import (
+    FullyShardedDataParallel as FSDP,
+    ShardingStrategy
+)
+from typing import Optional, Tuple
+
+from .optim import DeToNATION
+from .repl import Replicator
+
+__all__ = ["prepare_detonation"]
+
+def prepare_detonation(
+    model: torch.nn.Module,
+    replicator: Replicator,
+    sharding_group_size: Optional[int] = None,
+    replication_group_size: Optional[int] = None,
+    **kwargs,
+) -> Tuple[torch.nn.Module, torch.optim.Optimizer]:
+    world_size = int(os.environ['WORLD_SIZE'])
+    local_world_size = int(os.environ['LOCAL_WORLD_SIZE'])
+    if sharding_group_size is None:
+        if replication_group_size is None:
+            sharding_group_size = local_world_size
+            replication_group_size = world_size // local_world_size
+        else:
+            sharding_group_size = world_size // replication_group_size
+    elif replication_group_size is None:
+        replication_group_size = world_size // sharding_group_size
+    assert world_size == sharding_group_size * replication_group_size
+    assert local_world_size % sharding_group_size == 0
+    mesh_2d = init_device_mesh(
+        device_type="cuda",
+        mesh_shape=(sharding_group_size, replication_group_size),
+    )
+    model = FSDP(
+        model,
+        device_mesh=mesh_2d,
+        sharding_strategy=ShardingStrategy.HYBRID_SHARD,
+    )
+    optim = DeToNATION(model.parameters(), replicator=replicator, **kwargs)
+    return model, optim
