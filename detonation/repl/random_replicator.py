adde3abc6dcb1f0eccf91c69a8761105129e624e
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2025-03-27 23:53:43 +0100

    remove debug

--- detonation/repl/random_replicator.py
+++ detonation/repl/random_replicator.py
@@ -35,8 +35,6 @@ class RandomReplicator(Replicator):
         device = optim.param_groups[0]["params"][0].device
         self.random_state = torch.Generator(device=device).manual_seed(self.seed)
         self.sizes = set(p.view(-1, self.compression_chunk).size(0) for group in optim.param_groups for p in group["params"] if p.requires_grad)
-        print(self.sizes)
-        1/0
         for group in optim.param_groups:
             for p in group["params"]:
                 if p.requires_grad:
