005df84fc7077025cc4a48b0616819c76245852c
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2025-04-02 09:31:41 +0200

    refactoring with mixin needs testing

---
+++ detonation/optim/DeToSGD.py
@@ -0,0 +1,46 @@
+import torch
+import torch.distributed as dist
+import torch.fft
+from torch.optim import SGD
+from typing import Callable, List
+
+from ..detonatiomixin import DeToNATIONMixin
+from ..repl import DeMoReplicator, Replicator
+
+__all__ = ["DeToAdamW"]
+
+class DeToAdamW(SGD, DeToNATIONMixin):
+
+    def __init__(
+        self,
+        params,
+        detonation_weight_decay: float = 0.0,
+        detonation_sign: bool = True,
+        sharding_parallel_group: dist.ProcessGroup | None = None,
+        replication_parallel_group: dist.ProcessGroup | List[dist.ProcessGroup] | None = None,
+        replicator: Replicator | List[Replicator] = DeMoReplicator(),
+        replicate_every: int | List[int] = 1,
+        skip_every: int | List[int] | None = None,
+        *args,
+        **kwargs,
+    ):
+        SGD.__init__(
+            self,
+            params,
+            *args,
+            **kwargs,
+        )
+        DeToNATIONMixin.__init__(
+            self,
+            detonation_weight_decay=detonation_weight_decay,
+            detonation_sign=detonation_sign,
+            sharding_parallel_group=sharding_parallel_group,
+            replication_parallel_group=replication_parallel_group,
+            replicator=replicator,
+            replicate_every=replicate_every,
+            skip_every=skip_every,
+        )
+
+    @torch.no_grad()
+    def step(self, closure: Callable | None = None):
+        return DeToNATIONMixin.step(self, closure=closure, base_step=SGD.step)
